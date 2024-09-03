d1da766e4404c268b25fd36954abf4dec575912d
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-09-03 20:35:35 +0200

    FindMatching Class -> tests added

--- src/test/java/org/example/FindMatching/FindMatchingTest.java
+++ src/test/java/org/example/FindMatching/FindMatchingTest.java
@@ -10,15 +10,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FindMatchingTest {
 
+    List <Double> testProvidedData;
+    void setup () {
+        testProvidedData = List.of(10.5, 15.45, 5.811, 175.0);
+    }
+
     @Test
-    @DisplayName("Test double cisel s limitem mezi 4.0 a 9.3")
+    @DisplayName("The value is selected on the basis of the limit set by parameter")
     public void findMatching(){
-        Double testResult = FindMatching.findMatching(List.of(10.5, 15.45, 5.811, 175.0),4.0,9.3);
+        Double testResult = FindMatching.findMatching(testProvidedData,4.0,9.3);
         Double expected = 5.811;
 
         Assertions.assertEquals(expected,testResult);
     }
 
+    @Test
+    @DisplayName("The value is equal to the number from the list")
+    public void findMatchingEqualValue(){
+        Double testResult = FindMatching.findMatching(testProvidedData,4.0,10.0);
+        Double expected = 10.0;
+
+        Assertions.assertEquals(expected,testResult);
+    }
+
+    @Test
+    public void FindMatchingNullvalue () {
+        Double testResult = FindMatching.findMatching(testProvidedData,1.0,1.0);
+        Double expected = null;
+
+        Assertions.assertEquals(expected,testResult);
+    }
 
 
 }
