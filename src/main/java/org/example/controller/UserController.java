271fadcb196a98ba67778f51916441ff6326a7ac
Katerina Hodslavska <hodslavska.k@gmail.com>
2024-09-06 22:47:34 +0200

    Updated UserController by adding @GetMapping and @PostMapping annotations. Tested the functionality in Postman to verify if the correct values are returned based on the created users.

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -2,9 +2,7 @@ package org.example.controller;
 
 import org.example.User.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,11 +11,24 @@ import java.util.List;
 @RequestMapping("/users")
 public class UserController {
 
-    @GetMapping("/v1")
-    public List<User> getUsers(){
-        List<User> users = new ArrayList<>();
-        User user = new User();
+    /*  #Functional API Testing#
 
+    This UserController is a simple REST controller in Spring Boot that manages a list of users for learning purposes (without a database). It provides:
+
+    GET /users/create: Adds three default users to a list.
+    GET /users: Returns the list of all users.
+    GET /users/{id}: Returns a placeholder user with the specified ID.
+    POST /users: Adds a new user from the request body to the list.
+    In a real-world scenario, these actions would interact with a database.
+
+    -> Testing via API Postman
+     */
+
+
+    List<User> users = new ArrayList<>();
+
+    public void createDefaultUsers() {
+        User user = new User();
 
         user.setName("Katerina");
         user.setSurname("Hods");
@@ -30,9 +41,32 @@ public class UserController {
         users.add(user);
         users.add(user2);
         users.add(user3);
+    }
 
+
+    @GetMapping("/create")
+    public void createUsers(){
+        createDefaultUsers();
+    }
+
+    @GetMapping
+    public List<User> getUsers(){
         return users;
+    }
 
+
+    //these 'users' would be fetched from a database -> only for learning purpose now
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable("id") Long id){
+    User user = new User("User","ID",id, null);
+    return user;
+    }
+
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        users.add(user);
+        return user;
     }
 
 }
